30d7f363ebe1df2f6d92ce742e5dd0f2d4ce1c16
Chris <cbeley42@gmail.com>
2020-03-30 21:05:26 +1300

    3

--- Flicker Fusion/src/main.cpp
+++ Flicker Fusion/src/main.cpp
@@ -1,9 +1,47 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+/*
+Flicker Fusion 2020
+Chris Eley
+*/
+
+#define LEDPin 13
+#define ButtonPin 5
+
+const uint32_t debouncetime_ms = 150;
+
+uint8_t LEDState = false;
+uint8_t CurrButtonState = LOW;
+uint8_t PrevButtonState = LOW;
+uint32_t ButtonCount = 0;
+
+
+void setup() {                                                         
+  Serial.begin(9600);             //initialises serial interface and sets Baud rate (115200) 
+  pinMode(LEDPin, OUTPUT);
+  pinMode(ButtonPin, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static uint32_t PrevTime_ms = 0;
+  uint32_t        CurrTime_ms = millis();
+
+  CurrButtonState = !digitalRead(ButtonPin);
+  
+  //detect rising edge and debounce time
+  if((CurrButtonState == HIGH) && (PrevButtonState == LOW) && (CurrTime_ms - PrevTime_ms > debouncetime_ms)) {
+    //toggle LED
+    LEDState = !LEDState; 
+    digitalWrite(LEDPin, LEDState);
+    Serial.print("LEDState =");
+    Serial.println(LEDState);
+    //ButtonCount = (ButtonCount + 1);         //either add to buttoncount this way or us ++ButtonCount
+    Serial.print("ButtonCount =");
+    Serial.println(++ButtonCount);
+    PrevTime_ms = CurrTime_ms;
+  }
+  
+  PrevButtonState = CurrButtonState;
+  
+
 }
