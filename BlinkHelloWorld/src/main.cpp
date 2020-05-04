35521caa37008780f6f2bce1dd1e0656d04f472e
Chris <cbeley42@gmail.com>
2020-05-05 11:27:51 +1200

    200505

--- BlinkHelloWorld/src/main.cpp
+++ BlinkHelloWorld/src/main.cpp
@@ -13,7 +13,7 @@ Pin 6  has the LED on Teensy++ 2.0
 Pin 13 has the LED on Teensy 3.0
 give it a name: */
 
-const uint8_t led = 13;     //unsigned integer 8 bit data type- designating pin, no negative pins, limited number of pins
+const uint8_t led = 6;     //unsigned integer 8 bit data type- designating pin, no negative pins, limited number of pins
 //#define led 13                //alternate way of setting 13 to LED- uses less memory, pin number cannot be changed iin code
 
 // the setup routine runs once when you press reset:
