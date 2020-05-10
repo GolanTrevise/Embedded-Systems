6df145e3d11389a12f542180f5e7b4773ea60e83
Chris <cbeley42@gmail.com>
2020-05-10 20:12:06 +1200

    Change using analog out to builtin LED

--- Flicker Fusion/src/main.cpp
+++ Flicker Fusion/src/main.cpp
@@ -1,50 +1,55 @@
 #include <Arduino.h>
 
-#define led 13
-#define ledPin 6
+/*
+Ass. 0 Flicker Fusion
+By Chris Eley
+*/
+
+// Assigning the required pins
+#define ledPin 13                                       
 #define buttonPin 12
 
-//function prototype
-void ISR_Button();
+// Global Variables                     
+uint32_t freq;                                          // Creating the variables that require a global scope
+uint8_t ledState = LOW;                                 // Setting the LED state to low prior to setup
 
-void setup()
-{                
-  Serial.begin(115200);
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW);
-  attachInterrupt(buttonPin, ISR_Button, FALLING);
+// Function prototype for the Interrupt Service Routine
+void ISR_Button();                                      
 
+// Setup
+void setup() {
+  Serial.begin(115200);                                 // Initialises serial interface and sets Baud rate (115200) 
+  pinMode(buttonPin, INPUT_PULLUP);                     // Assigning the button pin Input status as well as activating the internal pull-up resistor
+  pinMode(ledPin, OUTPUT);                              // Assigning the LED Pin output status
+  digitalWrite(ledPin, ledState);                       // Ensures the LED is off prior to beginning the program
+  attachInterrupt(buttonPin, ISR_Button, RISING);       // Initiallising the Interrupt function
+}
+
+// Main Program Loop
+void loop() {
+  static uint32_t prevTime_us = 0;                      // Creating a local variable to store the previous time (in micro seconds)
+  uint32_t Pby2;                                        // A local variable to store the half cycle time of the LED
+
+  Pby2 = (analogRead(0)*30);                            // Reads the value of the 10k potentiometer and multiplies by 30 to get the half cycle time in microseconds
+  freq = (1000000 / (2*Pby2));                          // Calculates and stores the frequency of the LED
+
+  if (micros() - prevTime_us >= Pby2) {                 // Checks if the set interval has passed  
+    ledState = !ledState;                               // If conditions filled alters state of LED flage
+    digitalWrite(ledPin,ledState);                      // Writes the LED flag state to the LED pin.
+    prevTime_us = micros();                             // Sets the previous time to the current time to allow the cycle to start again                          
+  }
   
 }
 
-int32_t freq;
-
-void loop()                     
-{
-  int32_t val;
-  
-  static uint32_t prevMicros = 0;
-
-  
-  
-  val = analogRead(0);
-  freq = val/4;
-  /*Serial.print("analog 0 is: ");
-  Serial.println(val);
-  Serial.print("Freq is: ");
-  Serial.print(freq);
-  Serial.println("Hz");*/
-  analogWriteFrequency(ledPin, freq);
-  analogWrite(ledPin, 123);
-  //while((micros() - prevMicros) <= 100){  }
-  delay(100);
-  prevMicros = micros();
-  
-}
-
+// Interrupt service routine
 void ISR_Button(){
-  Serial.print("Freq is: ");
-  Serial.print(freq);
-  Serial.println("Hz");
+  static uint32_t prevMillisButton = 0;                 // Creates variable for debouncing inside the ISR 
+  uint32_t currMillisButton = millis();                 // Stores current time upon entering ISR
+  uint32_t debounceTime_ms = 20;                        // Sets the debounce time
+    if ((currMillisButton - prevMillisButton) >= debounceTime_ms){    // Debounce check
+      prevMillisButton = currMillisButton;              // Syncs the previous time the button was pushed to this button push
+      Serial.print("Freq is: ");                        
+      Serial.print(freq);                               // Prints current frequency to serial monitor
+      Serial.println(" Hz");
+    }   
 }
-
